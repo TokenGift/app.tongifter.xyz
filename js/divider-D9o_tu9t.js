e9871385bf040f2ff998864e32cb106554e27378
0x59014 <0x59014@gmail.com>
2024-11-26 14:05:39 +0800

    3

---
+++ js/divider-D9o_tu9t.js
@@ -0,0 +1,1 @@
+import{m as e,e as t,R as n,f as i}from"./index-CXKFpczX.js";const o="adm-divider",c={contentPosition:"center",direction:"horizontal"},r=r=>{const s=e(c,r);return t(s,n.createElement("div",{className:i(o,`${o}-${s.direction}`,`${o}-${s.contentPosition}`)},s.children&&n.createElement("div",{className:`${o}-content`},s.children)))};export{r as D};
