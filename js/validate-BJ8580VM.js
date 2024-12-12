9c9c8a80bbf8ef1bcd1e215a5f3e571e65f3f492
0x59014 <0x59014@gmail.com>
2024-12-12 13:42:16 +0800

    32

---
+++ js/validate-BJ8580VM.js
@@ -0,0 +1,1 @@
+import{V as t}from"./index-ChUO8asy.js";function o(t){return!!t&&"object"==typeof t&&"function"==typeof t.then}function e(){return!!t&&/ios|iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase())}export{e as a,o as i};
