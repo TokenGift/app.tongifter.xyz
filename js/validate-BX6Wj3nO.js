4695b3892e795ab0f5e94238a387ab09163eee0e
0x59014 <0x59014@gmail.com>
2024-12-18 11:52:23 +0800

    2

---
+++ js/validate-BX6Wj3nO.js
@@ -0,0 +1,1 @@
+import{W as t}from"./index-DdOA5bYa.js";function o(t){return!!t&&"object"==typeof t&&"function"==typeof t.then}function e(){return!!t&&/ios|iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase())}export{e as a,o as i};
